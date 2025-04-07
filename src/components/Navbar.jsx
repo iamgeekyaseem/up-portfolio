d5d4b8136a43ead16aba1c7678ecb1c53189498e
Aseem Gupta <iamgeekyaseem@gmail.com>
2025-04-07 13:20:03 +0530

    added all the files

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -10,7 +10,7 @@ export const Navbar = ({ menuOpen, setMenuOpen }) => {
       <div className="max-w-5xl mx-auto px-4">
         <div className="flex justify-between items-center h-16">
           <a href="#home" className="font-mono text-xl font-bold text-white">
-            iamGeeky<span className="text-blue-500">Aseem</span>
+            NanoBio<span className="text-blue-500">Innovate</span>
           </a>
 
           <div
