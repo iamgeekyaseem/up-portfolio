f33915e7b101d298ec9a6dac148d1f06a85c8d86
Aseem Gupta <iamgeekyaseem@gmail.com>
2025-04-07 21:05:05 +0530

    need to ad photos and final touch ups

--- src/components/sections/About.jsx
+++ src/components/sections/About.jsx
@@ -40,42 +40,7 @@ export const About = () => {
             an environmental engineer with my research focusing on
             nanotechnology for wastewater treatment.
           </p>
-          <div className="rounded-xl p-8 border-white/10 border hover:-translate-y-1 transition-all">
-            <p className="text-gray-300 mb-6">
-              I'm a web developer based in the India. I specialize in building
-              websites and web applications using modern web technologies.
-            </p>
-            <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
-              <div className="rounded-xl p-6 hover:-translate-y-1 transition-all">
-                <h3 className="text-xl font-bold mb-4">Frontend</h3>
-                <div className="flex flex-wrap gap-2">
-                  {frontendSkills.map((skill, key) => (
-                    <span
-                      key={skill}
-                      className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20 hover:shadow-[0_2px_8px_rgba(59, 130, 224, 0.2)] transition"
-                    >
-                      {skill}
-                    </span>
-                  ))}
-                </div>
-              </div>
-
-              <div className="rounded-xl p-6 hover:-translate-y-1 transition-all">
-                <h3 className="text-xl font-bold mb-4">Backend</h3>
-                <div className="flex flex-wrap gap-2">
-                  {backendSkills.map((skill, key) => (
-                    <span
-                      key={skill}
-                      className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20 hover:shadow-[0_2px_8px_rgba(59, 130, 224, 0.2)] transition"
-                    >
-                      {skill}
-                    </span>
-                  ))}
-                </div>
-              </div>
-            </div>
-          </div>
-
+          
           <div className="grid grid-row-1 md:grid-row-2 gap-6 mt-8">
             <div className="p-6 rounded-xl border-white/10 border hover:translate-y-1 transition-all">
               <h3 className="text-xl font-bold mb-4 text-blue-300">
