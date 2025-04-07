aa4060548ba9a77880a354cbd196262fc0df8630
Aseem Gupta <iamgeekyaseem@gmail.com>
2025-04-07 19:38:08 +0530

    bug fixed by removing reveal on scroll

--- src/components/sections/backup.jsx
+++ src/components/sections/backup.jsx
@@ -1,34 +1,13 @@
 import { RevealOnScroll } from "../RevealOnScroll";
 
 export const About = () => {
-  const frontendSkills = [
-    "HTML",
-    "CSS",
-    "JavaScript",
-    "React",
-    "Tailwind CSS",
-    "Sass",
-    "Figma",
-    "Photoshop",
-    "Pro Create",
-    "UI/UX Design",
-  ];
-
-  const backendSkills = [
-    "Node.js",
-    "Express",
-    "MongoDB",
-    "RESTful APIs",
-    "SQL",
-    "Python",
-  ];
-
+  
   return (
     <section
       id="about"
       className="min-h-screen flex items-center justify-center py-20"
     >
-     
+      <RevealOnScroll>
         <div className="max-w-3xl mx-auto px-4">
           <h2 className="text-3xl font-bold text-center mb-8 bg-gradient-to-r from-blue-500 to-cyan-400 bg-clip-text text-transparent text-center">
             About Me
@@ -430,7 +409,7 @@ export const About = () => {
             </div>
           </div>
         </div>
-      
+      </RevealOnScroll>
     </section>
   );
 };
