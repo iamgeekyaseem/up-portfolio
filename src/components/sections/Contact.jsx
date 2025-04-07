4822cb9d0c8988d59f2b04448332e3e4e9336698
Aseem Gupta <iamgeekyaseem@gmail.com>
2025-04-07 14:08:55 +0530

    added all the files

--- src/components/sections/Contact.jsx
+++ src/components/sections/Contact.jsx
@@ -9,19 +9,25 @@ export const Contact = () => {
       <RevealOnScroll>
         <div className="text-center z-10 px-4">
           <h2 className="text-3xl font-bold mb-6 bg-gradient-to-r from-blue-500 to-purple-200 bg-clip-text text-transparent leading-tight">
-            Contact Me
+            Connect and Collaborate:
           </h2>
-          <p className="text-gray-400 text-lg mb-8 max-w-lg mx-auto">
-            Feel free to reach out to me through the link below.
+          <p className="text-gray-400 text-justify text-lg mb-8 max-w-lg mx-auto">
+            I value the opportunity to connect with fellow scholars,
+            researchers, and enthusiasts who share a passion for knowledge and
+            discovery. Whether you have questions, insights to share, or
+            potential collaborations to explore, I invite you to reach out via
+            email at uddandaraopriyanka@gmail.com or connect with me on
+            LinkedIn. Thank you for visiting, and I look forward to engaging in
+            meaningful conversations and collaborations with you.
           </p>
           <div className="flex justify-center">
             <a
-              href="https://linktr.ee/thegeekyguy"
+              href="https://www.linkedin.com/in/uddandarao-priyanka-phd-039596177/"
               target="_blank"
               rel="noopener noreferrer"
               className="bg-blue-500 text-white py-3 px-6 rounded font-medium transition relative overflow-hidden hover:-translate-y-0.5 hover:shadow-[0_0_15px_rgba(59, 130, 256, 0.4)]"
             >
-              Contact Me
+              LinkedIn
             </a>
           </div>
         </div>
--- src/components/sections/Home.jsx
+++ src/components/sections/Home.jsx
@@ -4,39 +4,51 @@ export const Home = () => {
   return (
     <section
       id="home"
-      className="min-h-screen flex items-center justify-center relative "
+      className="min-h-screen flex items-center justify-center relative px-4"
     >
-      <RevealOnScroll>
-        <div className="text-center z-10 px-4">
-          <h1 className="text-5xl md:text-7xl font-bold mb-6 bg-gradient-to-r from-blue-500 to-purple-200 bg-clip-text text-transparent leading-tight">
-            Hi, I'm Uddandarao Priyanka
-          </h1>
-          <p className="text-gray-400 text-lg mb-8 max-w-4xl mx-auto text-justify">
-            Welcome to my personal blog, a digital space where I share insights,
-            discoveries, and reflections from my journey as an academician and a
-            researcher. I'm Dr. UDDANDARAO PRIYANKA, a dedicated scholar
-            passionate about exploring the intersections of Photosynthetic
-            chalcogenide nanobiohybrids for wastewater treatment and production
-            of value added products.
-          </p>
-          
-          <div className="flex justify-center space-x-4">
-            <a
-              href="#projects"
-              className="bg-blue-500 text-white py-3 px-6 rounded font-medium transition relative overflow-hidden hover:-translate-y-0.5 hover:shadow-[0_0_15px_rgba(59, 130, 256, 0.4)]"
-            >
-              View Projects
-            </a>
-
-            <a
-              href="#contact"
-              className="border border-blue-500/50 text-blue-500 py-3 px-6 rounded font-medium transition-all duration-200 hover:-translate-y-0.5 hover:shadow-[0_0_15px_rgba(59, 130, 256, 0.2)] hover:bg-blue-500/10"
-            >
-              Contact Me
-            </a>
-          </div>
+      <div className="max-w-7xl w-full flex flex-col md:flex-row items-center justify-center gap-10">
+        
+        {/* Left: Profile Image */}
+        <div className="w-full md:w-1/2 flex justify-center">
+          <img
+            src= 'src/assets/female-avatar.svg'
+            alt="Priyanka"
+            className="rounded-2xl shadow-xl w-72 h-72 object-cover"
+          />
         </div>
-      </RevealOnScroll>
+
+        {/* Right: Text Content */}
+        <RevealOnScroll>
+          <div className="w-full  text-center md:text-left z-10">
+            <h1 className="text-5xl md:text-7xl font-bold mb-6 bg-gradient-to-r from-blue-500 to-purple-200 bg-clip-text text-transparent leading-tight">
+              Hi, I'm Uddandarao Priyanka
+            </h1>
+            <p className="text-gray-400 text-lg mb-8 max-w-4xl text-justify">
+              Welcome to my personal blog, a digital space where I share insights,
+              discoveries, and reflections from my journey as an academician and a
+              researcher. I'm a dedicated scholar
+              passionate about exploring the intersections of Photosynthetic
+              chalcogenide nanobiohybrids for wastewater treatment and production
+              of value added products.
+            </p>
+
+            <div className="flex flex-col sm:flex-row gap-4 justify-center md:justify-start">
+              <a
+                href="#projects"
+                className="bg-blue-500 text-white py-3 px-6 rounded font-medium transition relative overflow-hidden hover:-translate-y-0.5 hover:shadow-[0_0_15px_rgba(59,130,246,0.4)]"
+              >
+                View Projects
+              </a>
+              <a
+                href="#contact"
+                className="border border-blue-500/50 text-blue-500 py-3 px-6 rounded font-medium transition-all duration-200 hover:-translate-y-0.5 hover:shadow-[0_0_15px_rgba(59,130,246,0.2)] hover:bg-blue-500/10"
+              >
+                Contact Me
+              </a>
+            </div>
+          </div>
+        </RevealOnScroll>
+      </div>
     </section>
   );
 };
